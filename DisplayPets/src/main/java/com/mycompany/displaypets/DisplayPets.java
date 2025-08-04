a903485751c6d8198423e4d7d8fa6d255e0076cf
qu33nwh0 <missmhinkley@gmail.com>
2025-08-04 17:05:54 +0800

    Initialisation

---
+++ DisplayPets/src/main/java/com/mycompany/displaypets/DisplayPets.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.displaypets;
+
+/**
+ *
+ * author Melanie Hinkley
+ */
+public class DisplayPets {
+
+    public static void main(String[] args) {
+        String puppyVariable = "Puppy";
+        String kittyVariable = "Kitty";
+        String bunnyVariable = "Bunny";
+        
+        System.out.println(puppyVariable);
+        System.out.println(puppyVariable);
+        
+        System.out.println(kittyVariable);
+        System.out.println(kittyVariable);
+        
+        
+        System.out.println(kittyVariable);
+        System.out.println(bunnyVariable);
+    }
+}
